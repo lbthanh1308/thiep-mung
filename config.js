8805116bb8c3201e16f69167b0e4db69d36cbfaa
lbthanh1308 <86762114+lbthanh1308@users.noreply.github.com>
2021-11-17 20:33:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'Thu Hiền';
+const nameGirl = 'Ngọc Quỳnh';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Tặng em';
-const titleCard = 'Làm người yêu anh hông';
-const contentCard = 'Gửi em cô bé vớt được từ liên quân, sợ anh thì sợ vừa vừa thui anh hông có làm gì bé đâu, anh thích tính cách cảu em, tính khuôn mặt của em, và thích luôn việc YÊU EM, cho 1 cơ hội nhá';
+const titleCard = 'Qùa của dân IT hơi sến';
+const contentCard = ' Xin lỗi vì để em đợi lâu, đây là món quà đầu tiên xem như lần đầu quen ha, nếu có duyên thì hẹn gặp em ở Sài Gòn';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
